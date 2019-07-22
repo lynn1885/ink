ea814031baf96ad537f8d6fca75df4f54b47364e
姬恒 <437462684@qq.com>
2019-07-22 19:41:46 +0800

    remove: some files

--- tools/createDir.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const dirs = ['threading', '_thread', 'random', 'BaseManager'];
-
-for (let d of dirs) {
-  fs.mkdirSync(d);
-  fs.writeFileSync(`./${d}/${d}.md`, '# 基础/总结');
-}
